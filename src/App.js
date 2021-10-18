1d8b15cb96ae80903266ce6d039f4009220129b2
Mikkail04 <57140188+Mikkail04@users.noreply.github.com>
2021-10-18 17:08:59 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello Ladies and Gentlemen</h1>
+      <h2>There are 2 h1's present, 2 file is shown, and 2 lines of code.</h2>
+    </div>
+  );
+}
